d3d370b10049e7456689b878798f21c2a7e299cc
colorfulbird233 <a1396228851@outlook.com>
2026-08-11 22:18:09 +0800

    Auto sync source code for Vercel

--- data/projects.ts
+++ data/projects.ts
@@ -11,14 +11,11 @@ export type Project = {
 
 export const projectsData: Project[] = [
   {
-    "id": "proj_1775049332705",
-    "name": "Computational Chemistry Tool",
-    "githubUrl": "https://github.com/heiehiehi/Computational_Chemistry_Tool",
-    "description": "该工具本作者使用在Win下的WSL2平台，系统为Ubuntu22，个人使用请依据自己数据进行修改（这些工具只是整合了一些流程）",
+    "id": "proj_1786457864721",
+    "name": "Darkwood Multiplayer Framework — 基于 BepInEx 与 Mirror/Telepathy 的 Darkwood 非官方多人联机框架。",
+    "githubUrl": "https://github.com/colorfulbird3/Darkwood-Multiplayer-framework/tree/main",
+    "description": "Darkwood Multiplayer Framework — 基于 BepInEx 与 Mirror/Telepathy 的 Darkwood 非官方多人联机框架。",
     "icon": "🚀",
-    "tags": [
-      "Gromacs",
-      "RMSF"
-    ]
-  },
+    "tags": []
+  }
 ];
